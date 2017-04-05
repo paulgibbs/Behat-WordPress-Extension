9fa6ec1da22c08d4a02ad75172111a9e768ab724
Paul Gibbs <djpaul@gmail.com>
2017-04-05 15:34:51 +0100

    Driver element: update interface with new params and docs.

--- src/Driver/Element/ElementInterface.php
+++ src/Driver/Element/ElementInterface.php
@@ -14,21 +14,33 @@ interface ElementInterface
 {
     /**
      * Create an item for this element.
+     *
+     * @param array $args Data used to create an object.
+     * @return int|string New object ID.
      */
-    public function create();
+    public function create($args);
 
     /**
      * Retrieve an item for this element.
+     *
+     * @param int|string $id   Object ID.
+     * @param array      $args Data used to fetch an object.
      */
-    public function get();
+    public function get($id, $args);
 
     /**
      * Update an item for this element.
+     *
+     * @param int|string $id   Object ID.
+     * @param array      $args Data used to update an object.
      */
-    public function update();
+    public function update($id, $args);
 
     /**
      * Delete an item for this element.
+     *
+     * @param int|string $id   Object ID.
+     * @param array      $args Data used to delete an object.
      */
-    public function delete();
+    public function delete($id, $args);
 }
--- src/Driver/Element/Wpcli/ContentElement.php
+++ src/Driver/Element/Wpcli/ContentElement.php
@@ -4,35 +4,47 @@ namespace PaulGibbs\WordpressBehatExtension\Driver\Element\Wpcli;
 use PaulGibbs\WordpressBehatExtension\Driver\Element\BaseElement;
 
 /**
- * yolo.
+ * WP-CLI driver element for content (i.e. blog posts).
  */
 class ContentElement extends BaseElement
 {
     /**
      * Create an item for this element.
+     *
+     * @param array $args Data used to create an object.
+     * @return int|string New object ID.
      */
-    public function create()
+    public function create($args)
     {
     }
 
     /**
      * Retrieve an item for this element.
+     *
+     * @param int|string $id   Object ID.
+     * @param array      $args Data used to fetch an object.
      */
-    public function get()
+    public function get($id, $args)
     {
     }
 
     /**
      * Update an item for this element.
+     *
+     * @param int|string $id   Object ID.
+     * @param array      $args Data used to update an object.
      */
-    public function update()
+    public function update($id, $args)
     {
     }
 
     /**
      * Delete an item for this element.
+     *
+     * @param int|string $id   Object ID.
+     * @param array      $args Data used to delete an object.
      */
-    public function delete()
+    public function delete($id, $args)
     {
     }
 }
