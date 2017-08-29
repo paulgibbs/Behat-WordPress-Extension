bb86b5c5f69f7384540bda576e4328efd43a7863
Paul Gibbs <djpaul@gmail.com>
2017-08-29 11:55:58 +0100

    Don't install all thigns with WebPI

--- bin/appveyor/install1.ps1
+++ bin/appveyor/install1.ps1
@@ -3,22 +3,18 @@
 ## Install WebPI.
 cInst webpicommandline -y
 
-# Install PHP and MySQL.
-$webPiProducts = @('UrlRewrite2', 'MySQL', 'PHP56', 'WordPress')
-WebPICMD /Install /Products:"$($webPiProducts -join ',')" /AcceptEULA /MySQLPassword:root
+# Install environment dependencies via WebPI.
+WebPICMD /Install /Application:"PHP70" /AcceptEULA
 
+echo (Get-Website -Name "Default Web Site").PhysicalPath
+echo (get-webapplication foo).PhysicalPath
 echo @php %%~dp0composer.phar %%* > composer.bat
 appveyor DownloadFile https://getcomposer.org/composer.phar
 
-# Start required Windows services.
+# Start Windows services for IIS.
 Start-Service W3SVC
 Start-Service WAS
 
 # Install WordHat.
 cd $APPVEYOR_BUILD_FOLDER
 composer install --no-interaction --prefer-dist --no-progress
-
-# Download WordPress.
-cd $APPVEYOR_BUILD_FOLDER
-mkdir c:\projects\wordpress
-#vendor\bin\wp core download --force --version=latest --path=c:\projects\wordpress
